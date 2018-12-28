556693a3d69680ddfd0e292aebdcc76ee632d91c
David Lukava <dflukava@gmail.com>
2018-12-28 22:03:51 +0300

    add comments for usage

--- tp.js
+++ tp.js
@@ -2,6 +2,6 @@ const fs = require('fs');
 
 let pl = fs.readFileSync('./proxylist', 'utf8');
 pl = JSON.parse(pl);
-pl = pl.splice(0, 600)
+pl = pl.splice(0, 600)//how much proxies u wanna get in result 0->600 here
 fs.appendFileSync('./proxylist2', JSON.stringify(pl) ,'utf8')
 console.log(pl)
